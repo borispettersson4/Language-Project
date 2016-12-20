9e4a6a2418b6fd78407485837cbf6f53cf1f29be
borisruiz4 <borisruiz4@gmail.com>
2016-12-20 16:16:59 -0400

    Added extra libraries

--- Language Interpreter/src/language/interpreter/Identifier.java
+++ Language Interpreter/src/language/interpreter/Identifier.java
@@ -50,16 +50,120 @@ public class Identifier {
       return x;
     }
    
-   public int variableType(){
+    public int variableType(){
        int i = 0;
        if(isVariable()){
            if((body.charAt(0) >= 'a' && body.charAt(0) <= 'f') || (body.charAt(0) >= 'A' && body.charAt(0) <= 'F'))
                i = 1;
-           if((body.charAt(0) >= 'g' && body.charAt(0) <= 'n') || (body.charAt(0) >= 'G' && body.charAt(0) <= 'N'))
+           else if((body.charAt(0) >= 'g' && body.charAt(0) <= 'n') || (body.charAt(0) >= 'G' && body.charAt(0) <= 'N'))
                i = 2;
-           if((body.charAt(0) >= 'o' && body.charAt(0) <= 'z') || (body.charAt(0) >= 'O' && body.charAt(0) <= 'Z'))
+           else if((body.charAt(0) >= 'o' && body.charAt(0) <= 'z') || (body.charAt(0) >= 'O' && body.charAt(0) <= 'Z'))
                i = 3;
        }
        return i;
    }
+   
+    public boolean isInteger() {
+    try { 
+        Integer.parseInt(body); 
+    } catch(NumberFormatException e) { 
+        return false; 
+    } catch(NullPointerException e) {
+        return false;
+    }
+    // only got here if we didn't return false
+    return true;
+}
+    
+    public int convertToInt() {
+    return Integer.parseInt(body);
+    }
+    
+      public boolean isDouble() {
+    try { 
+        Double.parseDouble(body); 
+    } catch(NumberFormatException e) { 
+        return false; 
+    } catch(NullPointerException e) {
+        return false;
+    }
+    // only got here if we didn't return false
+    return true;
+}
+    
+    public double convertToDouble() {
+    return Double.parseDouble(body);
+    }
+       
+    public boolean isSignedInteger(){
+        return (Integer.parseInt(body) < 0);
+    }
+    
+    public boolean isSignedDouble(){
+        return (Double.parseDouble(body) < 0);
+    }
+   
+    public boolean isOperator() {
+      boolean x = false;
+      
+      if(body.contains(".or.") || body.contains(".and.") || body.contains(".not.") || body.contains(".eq.") || body.contains(".ne.") || body.contains(".lt.") || body.contains(".le.") || body.contains(".gt.") || body.contains(".ge."))
+          x = true;
+      
+      return x;
+   }
+   
+   public int OperatorType(){
+       int i = 0;
+       if(isOperator()){
+           if(body.contains(".or."))
+               i = 1;
+           else if(body.contains(".and."))
+               i = 2;
+           else if(body.contains(".not."))
+               i = 3;
+           else if(body.contains(".eq."))
+               i = 4;
+           else if(body.contains(".ne."))
+               i = 5;
+           else if(body.contains(".lt."))
+               i = 6;
+           else if(body.contains(".le."))
+               i = 7;
+           else if(body.contains(".gt."))
+               i = 8;
+           else if(body.contains(".ge."))
+               i = 9;
+       }
+       return i;
+   }
+   
+   public boolean isMathOperator() {
+       boolean x = false;
+       if(body.contains(".add.") || body.contains(".sub.") || body.contains(".mul.") || body.contains(".div."))
+           x = true;
+       return x;
+   }
+   
+    public int MathOperatorType(){
+       int i = 0;
+       if(isMathOperator()){
+           if(body.contains(".add."))
+               i = 1;
+           else if(body.contains(".sub."))
+               i = 2;
+           else if(body.contains(".mul."))
+               i = 3;
+           else if(body.contains(".div."))
+               i = 4;
+       }
+       return i;
+   }
+   
+   public boolean isAssigner() {
+       boolean x = false;
+       if(body.contains("="))
+           x = true;
+       return x;
+   }
+   
 }
--- Language Interpreter/src/language/interpreter/LanguageInterpreter.java
+++ Language Interpreter/src/language/interpreter/LanguageInterpreter.java
@@ -17,39 +17,105 @@ public class LanguageInterpreter {
      */
     public static void main(String[] args) 
     {
+            String c;
+        do{
+        
        Identifier aElement = new Identifier();
        
        Scanner input = new Scanner(System.in);    
-       System.out.println("Enter a variable name");
+       System.out.println("Enter anything");
        String aString = input.nextLine();
+       aString.trim();
        aElement.setBody(aString);
        
- 
-       if (aString != null){
-           aString = " ";
+ //FailSafe
+       if (aString == ""){
            do{
            System.out.println("You've pressed enter, please try inserting a valid variable name: ");
            aString = input.nextLine();
            aElement.setBody(aString);
-           }while(aString == " ");
+           }while(aString == "");
        }
         
-       
-               
-       if(aElement.isVariable()) {
+       //Check if is integer
+       if(aElement.isInteger()) {
+           aElement.convertToInt();
+           if(aElement.isSignedInteger())
+           System.out.println("This is a signed Integer!"); 
+           else
+           System.out.println("This is an unsigned Integer!");  
+       }
+       //Check if is double
+       else if(aElement.isDouble()) {
+           aElement.convertToDouble();
+           if(aElement.isSignedDouble())
+           System.out.println("This is a signed Real Number!");   
+           else 
+           System.out.println("This is an unsigned Real Number!");   
+       }
+       //Check if is mathematical operator
+       else if(aElement.isMathOperator()) {
+          System.out.println("This is a mathematical operator!");
+       if(aElement.MathOperatorType() == 1)
+           System.out.println("This is addition");
+       else if(aElement.MathOperatorType() == 2)
+           System.out.println("This is substraction");
+       else if(aElement.MathOperatorType() == 3)
+           System.out.println("This is multiplication");
+       else if(aElement.MathOperatorType() == 4)
+           System.out.println("This is a division");
+       }
+           //Check if is assigner
+       else if(aElement.isAssigner()) {
+          System.out.println("This is the assigner operator!");
+       }
+          //Check if is operator
+       else if(aElement.isOperator()) {
+          System.out.println("This is an operator!");
+       if(aElement.OperatorType() == 1)
+           System.out.println("This is a logical OR operator");
+       else if(aElement.OperatorType() == 2)
+           System.out.println("This is a logical AND operator");
+       else if(aElement.OperatorType() == 3)
+           System.out.println("This is a logical NOT oprator");
+       else if(aElement.OperatorType() == 4)
+           System.out.println("This is a comparative EQUAL operator");
+       else if(aElement.OperatorType() == 5)
+           System.out.println("This is a comparative NOT EQUAL operator");
+       else if(aElement.OperatorType() == 6)
+           System.out.println("This is a comparative LESS THAN operator");
+       else if(aElement.OperatorType() == 7)
+           System.out.println("This is a comparative LESS EQUAL operator");
+       else if(aElement.OperatorType() == 8)
+           System.out.println("This is a comparative GREATER THAN operator");
+       else if(aElement.OperatorType() == 9)
+           System.out.println("This is a comparative GREATER EQUAL operator");
+       }
+       //Check if is variable
+       else if(aElement.isVariable()) {
           System.out.println("This is a variable!");
        if(aElement.variableType() == 1)
-           System.out.println("This is an Integer");
+           System.out.println("The variable type is Integer");
        else if(aElement.variableType() == 2)
-           System.out.println("This is a Real Number");
+           System.out.println("The variable type is Real Number");
        else if(aElement.variableType() == 3)
-           System.out.println("This is a String");
+           System.out.println("The variable type is String");
        }
        else 
-           System.out.println("This is not a variable");
-        
-       //if(aString != null)
-        //System.out.println("you've netered the enter key");
+           System.out.println("This is an unkown token");
+       
+       
+       
+       
+       
+       
+       //Final Verdict
         System.out.println("You've entered : " + aElement.getBody());
+               
+        System.out.println("");
+        
+        System.out.println("Would you like to try again (Y/N)");
+        c = input.nextLine();
+        }while(!(c.contains("N")|| c.contains("n")));
     }      
 }
